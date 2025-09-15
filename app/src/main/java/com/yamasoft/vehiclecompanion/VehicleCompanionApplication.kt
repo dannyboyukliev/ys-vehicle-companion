aeba45b73e8d9cc4872b3da1b88ce9381af055a0
Danail Boyukliev <danailboyukliev@gmail.com>
2025-09-15 23:29:59 +0300

    feat: setup DI

--- app/src/main/java/com/yamasoft/vehiclecompanion/VehicleCompanionApplication.kt
+++ app/src/main/java/com/yamasoft/vehiclecompanion/VehicleCompanionApplication.kt
@@ -1,7 +1,9 @@
 package com.yamasoft.vehiclecompanion
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class VehicleCompanionApplication: Application() {
     override fun onCreate() {
         super.onCreate()
