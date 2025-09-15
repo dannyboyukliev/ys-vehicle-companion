0937d15504215a62be95db72d55304b9fec15378
dannyboyukliev <uragann@windowslive.com>
2025-09-15 23:32:44 +0300

    feat: organize build.gradle dependencies and add core libraries (#2)
    * refactor: group and organize build.gradle dependencies
    
    * feat: add Kotlin Coroutines dependencies
    
    * feat: add KSP and Room dependencies
    
    * feat: setup Jetpack Compose Navigation library
    
    * feat: setup OkHttp+Retrofit for Networking and Moshi for JSON converter
    
    * feat: setup Hilt
    
    * feat: setup Application class
    
    * feat: setup DI
    
    ---------
    
    Co-authored-by: Danail Boyukliev <danailboyukliev@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlinAndroidKsp)
+    alias(libs.plugins.hiltPlugin)
 }
 
 android {
@@ -41,19 +43,49 @@ android {
 
 dependencies {
 
+    // Core Android libraries
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Database (Room)
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
+    // Compose BOM and UI libraries
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
+
+    // Dependency Injection
+    implementation (libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    // Networking
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.moshi)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+    implementation(libs.moshi)
+
+    // Unit testing
     testImplementation(libs.junit)
+
+    // Android instrumented testing
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
+    // Debug tools
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    
 }
