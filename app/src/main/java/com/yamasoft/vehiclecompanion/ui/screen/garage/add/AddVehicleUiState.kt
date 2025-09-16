2c9624ff4d212856941e89d9fefad0368ab352c2
Danail Boyukliev <danailboyukliev@gmail.com>
2025-09-16 08:38:24 +0300

    feat: add Add vehicle uiState

---
+++ app/src/main/java/com/yamasoft/vehiclecompanion/ui/screen/garage/add/AddVehicleUiState.kt
@@ -0,0 +1,8 @@
+package com.yamasoft.vehiclecompanion.ui.screen.garage.add
+
+data class AddVehicleUiState(
+    val name: String = "",
+    val make: String = "",
+    val model: String = "",
+    val year: String = ""
+)
