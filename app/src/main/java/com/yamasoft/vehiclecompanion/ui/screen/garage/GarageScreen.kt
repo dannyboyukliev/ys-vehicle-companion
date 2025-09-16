9f721b837014ae7a7e2a7ae9da1d9df69c92ce51
Danail Boyukliev <danailboyukliev@gmail.com>
2025-09-16 07:50:30 +0300

    feat: add Garage viewModel

--- app/src/main/java/com/yamasoft/vehiclecompanion/ui/screen/garage/GarageScreen.kt
+++ app/src/main/java/com/yamasoft/vehiclecompanion/ui/screen/garage/GarageScreen.kt
@@ -25,8 +25,21 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import com.yamasoft.vehiclecompanion.R
 import com.yamasoft.vehiclecompanion.ui.theme.VehicleCompanionTheme
+
+@Composable
+private fun GarageScreen(
+
+) {
+
+}
+
+@Composable
+private fun GarageScreenContent() {
+
+}
 
 @Composable
 private fun EmptyGarageState(
