b0bf51d9c355ace11d31a6e1e79d536a4e9c877d
Danail Boyukliev <danailboyukliev@gmail.com>
2025-09-17 00:53:53 +0300

    feat: add icons to bottom bar tabs

--- app/src/main/java/com/yamasoft/vehiclecompanion/ui/components/VehicleCompanionBottomBar.kt
+++ app/src/main/java/com/yamasoft/vehiclecompanion/ui/components/VehicleCompanionBottomBar.kt
@@ -1,9 +1,14 @@
 package com.yamasoft.vehiclecompanion.ui.components
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import com.yamasoft.vehiclecompanion.R
 import com.yamasoft.vehiclecompanion.ui.navigation.Route
 
 @Composable
@@ -15,14 +20,24 @@ fun VehicleCompanionBottomBar(
         NavigationBarItem(
             selected = currentRoute == Route.GARAGE,
             onClick = { onNavigate(Route.GARAGE) },
-            icon = {},
+            icon = {
+                Icon(
+                    painter = painterResource(R.drawable.directions_car),
+                    contentDescription = null
+                )
+            },
             label = { Text("Garage") }
         )
 
         NavigationBarItem(
             selected = currentRoute == Route.PLACES,
             onClick = { onNavigate(Route.PLACES) },
-            icon = {},
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Place,
+                    contentDescription = null
+                )
+            },
             label = { Text("Places") }
         )
     }
