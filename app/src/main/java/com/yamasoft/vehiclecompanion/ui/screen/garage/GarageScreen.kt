2a0a7b76036741ad3ea34a34e9bfe0562381b976
Danail Boyukliev <danailboyukliev@gmail.com>
2025-09-16 08:05:44 +0300

    feat: add Garage screen

--- app/src/main/java/com/yamasoft/vehiclecompanion/ui/screen/garage/GarageScreen.kt
+++ app/src/main/java/com/yamasoft/vehiclecompanion/ui/screen/garage/GarageScreen.kt
@@ -1,7 +1,9 @@
 package com.yamasoft.vehiclecompanion.ui.screen.garage
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,6 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -17,6 +20,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -25,20 +30,60 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.yamasoft.vehiclecompanion.R
+import com.yamasoft.vehiclecompanion.domain.model.Vehicle
+import com.yamasoft.vehiclecompanion.ui.components.VehicleCard
+import com.yamasoft.vehiclecompanion.ui.navigation.Route
 import com.yamasoft.vehiclecompanion.ui.theme.VehicleCompanionTheme
 
 @Composable
-private fun GarageScreen(
-
+fun GarageScreen(
+    navController: NavController,
+    viewModel: GarageViewModel = hiltViewModel()
 ) {
+    val uiState by viewModel.uiState.collectAsState()
 
+    GarageScreenContent(
+        uiState = uiState,
+        onAddVehicleClick = {}
+    )
 }
 
 @Composable
-private fun GarageScreenContent() {
+private fun GarageScreenContent(
+    uiState: GarageUiState,
+    onAddVehicleClick: () -> Unit,
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        when {
+            uiState.vehicles.isEmpty() -> {
+                EmptyGarageState(onAddVehicleClick = onAddVehicleClick)
+            }
 
+            // When there are vehicles
+            else -> {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    contentPadding = PaddingValues(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    items(
+                        count = uiState.vehicles.size,
+                    ) { index ->
+                        val vehicle = uiState.vehicles[index]
+                        VehicleCard(
+                            vehicle = vehicle,
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
 
 @Composable
@@ -103,6 +148,18 @@ private fun EmptyGarageState(
         }
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+private fun GarageScreenContentWithVehiclesPreview() {
+    VehicleCompanionTheme {
+        GarageScreenContent(
+            uiState = GarageUiState(listOf(Vehicle(name = "My Car", make = "Toyota", model = "Avensis", year = 2009))),
+            onAddVehicleClick = {}
+        )
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
